fdad061f28346a70c2a8fbefd1c64a7b11c8000e
John Morales <john@Johns-MBP.attlocal.net>
2022-12-20 12:32:37 -0800

    need to fix save button

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,6 +13,17 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
+  edit(member: object){
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+    } 
+    
   add(memberName: string, isFirst: boolean){
     this.crew.push({name: memberName, firstMission: isFirst}); 
   }
